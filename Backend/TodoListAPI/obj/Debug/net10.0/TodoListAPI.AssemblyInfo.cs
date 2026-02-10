e2a2f36f0c3a90f50e1b394918326c3ee9b29ee8
Sahan2002-cmd <it23837676@.my.sliit.lk>
2026-02-10 08:50:13 +0530

    File clean and setpu hiratical structure for the project.

--- Backend/TodoListAPI/obj/Debug/net10.0/TodoListAPI.AssemblyInfo.cs
+++ Backend/TodoListAPI/obj/Debug/net10.0/TodoListAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoListAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a9608356116bcb6b4c064765069f82368bca539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+129f86d2d86716c8983cda91ca5cb91ed02d511f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoListAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoListAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
